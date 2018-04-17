cd73fe348396cfd9c17339379dc0215cd8a31dcc
klay <hwc2580@126.com>
2018-04-17 16:34:34 +0800

    实现视频的收藏，并在个人主页展示收藏的视频

--- src/main/java/com/klay/controller/profileController.java
+++ src/main/java/com/klay/controller/profileController.java
@@ -2,9 +2,12 @@ package com.klay.controller;
 
 import com.klay.dao.CommentMapper;
 import com.klay.dao.UserMapper;
+import com.klay.dao.VideoLikeMapper;
 import com.klay.dao.VideoMapper;
 import com.klay.model.Comment;
 import com.klay.model.User;
+import com.klay.model.Video;
+import com.klay.model.VideoLike;
 import com.klay.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -34,6 +38,9 @@ public class profileController {
     @Autowired
     VideoMapper videoMapper;
 
+    @Autowired
+    VideoLikeMapper videoLikeMapper;
+
     @RequestMapping("/profile")
     public String profile(HttpSession session, Model model){
         model.addAttribute("username",session.getAttribute("sessionUsername"));
@@ -41,6 +48,21 @@ public class profileController {
         List<Comment> commentList = this.commentMapper.selectByUserId((String)session.getAttribute("sessionUsername"));
         model.addAttribute("commentList",commentList);
         model.addAttribute("user",user);
+        if (session.getAttribute("sessionUsername") == null){
+            return "index";
+        }
+        List<VideoLike> videoLikeList = this.videoLikeMapper.selectAll();
+        List<VideoLike> myVideoLikeList = new ArrayList<>();
+        List<Video> videoList = new ArrayList<>();
+        for (VideoLike videolike : videoLikeList) {
+            if(videolike.getUserId().equals(session.getAttribute("sessionUsername"))){
+                myVideoLikeList.add(videolike);
+            }
+        }
+        for (VideoLike videolike : myVideoLikeList) {
+            videoList.add(this.videoMapper.selectByPrimaryKey(videolike.getVideoId()));
+        }
+        model.addAttribute("videoList",videoList);
         return "profile";
     }
 
@@ -80,4 +102,42 @@ public class profileController {
         return "redirect:/profile";
     }
 
+    @ResponseBody
+    @RequestMapping("/addVideoLike")
+    public void addVideoLike(String videoId,HttpSession session){
+        String username = (String) session.getAttribute("sessionUsername");
+        VideoLike videoLike = new VideoLike();
+        videoLike.setUserId(username);
+        videoLike.setVideoId(videoId);
+        int add = this.videoLikeMapper.insert(videoLike);
+    }
+
+    @ResponseBody
+    @RequestMapping("/deleteVideoLike")
+    public void deleteVideoLike(String videoId,HttpSession session){
+        String username = (String) session.getAttribute("sessionUsername");
+        VideoLike videoLike = new VideoLike();
+        videoLike.setUserId(username);
+        videoLike.setVideoId(videoId);
+        int delete = this.videoLikeMapper.deleteByPrimaryKey(username,videoId);
+    }
+
+    @ResponseBody
+    @RequestMapping("/selectAllVideoLike")
+    public boolean selectAllVideoLike(String videoId,HttpSession session,Model model){
+        String username = (String) session.getAttribute("sessionUsername");
+        VideoLike videoLike1 = new VideoLike();
+        videoLike1.setVideoId(videoId);
+        videoLike1.setUserId(username);
+        List<VideoLike> videoLikeList = this.videoLikeMapper.selectAll();
+        boolean flag = false;
+        for (VideoLike videoLike:videoLikeList){
+            if(videoLike.getUserId().equals(videoLike1.getUserId())&&videoLike.getVideoId().equals(videoLike1.getVideoId()))
+            {
+                flag = true;
+            }
+        }
+        model.addAttribute("flag",flag);
+        return flag;
+    }
 }
